ca659269ac3b28149bc0f981afde340f3d0bf388
meshackkazimoto <meshackkazimoto@gmail.com>
2026-01-28 01:20:14 +0300

    trash

--- apps/api/src/modules/club/club.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Club } from './club.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Club])],
-  exports: [TypeOrmModule],
-})
-export class ClubModule {}
